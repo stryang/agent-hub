afbe0d745142bd93115c5562ac55a660496c9933
stryang <yangqiang968@gmail.com>
2026-06-13 02:04:44 +0800

    style: fix Hermes logo color, lighten accent-dim, apply accent to selector label

--- src/renderer/components/CliSelector.tsx
+++ src/renderer/components/CliSelector.tsx
@@ -61,10 +61,10 @@ export function CodexLogo() {
 export function HermesLogo() {
   return (
     <svg
-      fill="currentColor"
+      fill="#9046FF"
       fillRule="evenodd"
       height="1em"
-      style={{ flex: "none", lineHeight: 1, color: "var(--accent)" }}
+      style={{ flex: "none", lineHeight: 1 }}
       viewBox="0 0 24 24"
       width="1em"
       xmlns="http://www.w3.org/2000/svg"
